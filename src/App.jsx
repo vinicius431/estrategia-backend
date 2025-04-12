b69e02646cacd9f285e084c90c71609a3ce09f87
Vinicius Nogueira <viniciusalvessantosngueira@gmail.com>
2025-04-12 12:36:04 -0300

    Corrigido import React no App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 function App() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-blue-900 via-blue-800 to-blue-700 text-white font-sans">
