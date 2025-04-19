78692d2bad337f62cd5d0869f6d53de8d83c6a6a
Vinicius Nogueira <viniciusalvessantosngueira@gmail.com>
2025-04-19 08:28:48 -0300

    conecta frontend com backend do Render

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -12,7 +12,7 @@ export default function Login() {
     setMensagem("");
 
     try {
-      const res = await fetch("http://localhost:3001/auth/login", {
+      const res = await fetch(`${process.env.REACT_APP_API_URL}/auth/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, senha }),
