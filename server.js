1d8da71e5edaedc037cb1dd95e5fd94755710e48
Vinicius Nogueira <viniciusalvessantosngueira@gmail.com>
2025-05-31 12:52:58 -0300

    Removida rota duplicada instagramService.js; mantendo apenas instagramPublicar.js como endpoint oficial de publicação

--- server.js
+++ server.js
@@ -47,14 +47,18 @@ app.use(cors({
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
-const instagramRoutes = require("./routes/instagramService"); // ✅ ROTAS INSTAGRAM
-app.use("/api", instagramRoutes); // ✅ depois usa o app normalmente
+const instagramServiceRoutes = require("./routes/instagramService"); // ✅ ROTAS INSTAGRAM
+app.use("/api", instagramServiceRoutes); // ✅ depois usa o app normalmente
 
 const integracaoRoutes = require("./routes/integracao");
 app.use("/api", integracaoRoutes);
 
 const publicarInstagram = require("./routes/instagramPublicar");
 app.use("/api", publicarInstagram);
+
+const outrasRotasInstagram = require("./routes/instagram");
+app.use("/api", outrasRotasInstagram);
+
 
 
 const uploadRoute = require("./routes/upload");
