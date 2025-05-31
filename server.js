46e0a790ec44a482292f07cdb5779b9cfd0b619c
Vinicius Nogueira <viniciusalvessantosngueira@gmail.com>
2025-05-31 13:00:38 -0300

    🔥 Removida a rota duplicada instagramService e ajustado server.js

--- server.js
+++ server.js
@@ -47,8 +47,7 @@ app.use(cors({
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
-const instagramServiceRoutes = require("./routes/instagramService"); // ✅ ROTAS INSTAGRAM
-app.use("/api", instagramServiceRoutes); // ✅ depois usa o app normalmente
+
 
 const integracaoRoutes = require("./routes/integracao");
 app.use("/api", integracaoRoutes);
